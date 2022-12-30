d37e9bb55d7d135e1214834e2d66f00ca5712f33
NathanDBrowne <nathan.browne1@gmail.com>
2022-12-30 18:50:22 +0000

    add codec route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./routes/Home";
+import Codec from "./routes/Codec";
 import NotFound from "./routes/NotFound";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/codec" element={<Codec />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
