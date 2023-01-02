47fb99caacd1857844ead51cd07927d2aeb0fa74
NathanDBrowne <nathan.browne1@gmail.com>
2023-01-02 14:36:36 +0000

    create loading bar on re-route

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import "./styles.css";
+
+import { ScaleLoader } from "react-spinners";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="loader-container">
+      <ScaleLoader
+        color="#0dff4d"
+        loading={true}
+        // size={50}
+        cssOverride={{
+          display: "block",
+          margin: "0 auto",
+          borderColor: "red",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Loader;
